da87c90d0ec3144b96050daa3f1e8dcc89d1809b
Sharma <vipul.sharma@ebixnoida.com>
2024-06-20 17:49:12 +0530

    commit

---
+++ src/main/java/com/vipul/hrms/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.vipul.hrms.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String Deparment;
+
+	private Double Salary;
+
+	private String Location;
+}
